7d0882e8f81e6f701715ad80ba7bf3986a924ce7
Juha Ilo <jiidoe79@gmail.com>
2020-04-17 15:42:30 +0300

    Trying out stage size and positioning

--- src/main/java/com/company/App.java
+++ src/main/java/com/company/App.java
@@ -2,14 +2,21 @@ package com.company;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class App extends Application {
     @Override
     public void start(Stage stage) {
         stage.setTitle("Something else");
+        stage.initStyle(StageStyle.UTILITY);
+        stage.setWidth(640);
+        stage.setHeight(480);
+        stage.setX(600);
+        stage.setY(300);
         stage.show();
     }
-    public static void main(String args[]) {
+
+    public static void main(String args[]){
         launch(args);
     }
 }
