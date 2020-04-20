83ed8b5a29dd421b7e9ccf76f9e196199fb4c354
Juha Ilo <jiidoe79@gmail.com>
2020-04-20 10:38:11 +0300

    Button exercise

--- src/main/java/com/company/App.java
+++ src/main/java/com/company/App.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import javafx.application.Application;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
@@ -11,13 +12,19 @@ public class App extends Application {
     public void start(Stage stage) {
         stage.setTitle("Something else");
         stage.initStyle(StageStyle.UTILITY);
-        stage.setWidth(640);
-        stage.setHeight(480);
+        //stage.setWidth(640);
+        //stage.setHeight(480);
         stage.setX(600);
         stage.setY(300);
         stage.show();
-        Scene scene = new Scene(new Button("Click!"));
+        Button worldButton = new Button ("World");
+        //worldButton.setTranslateY( 400 - worldButton.getHeight());
+        //worldButton.setTranslateX( 600 - worldButton.getWidth());
+        Group group = new Group(new Button("Hello"), worldButton);
+        Scene scene = new Scene(group, 640, 480);
         stage.setScene(scene);
+        worldButton.setTranslateY( 480 - worldButton.getHeight());
+        worldButton.setTranslateX( 640 - worldButton.getWidth());
     }
 
     public static void main(String args[]){
