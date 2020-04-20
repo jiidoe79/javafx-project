d5c5626e631708cd6ba2f93ac30a31f27ad659ea
Juha Ilo <jiidoe79@gmail.com>
2020-04-20 11:13:45 +0300

    Menus added + exit function

--- src/main/java/com/company/App.java
+++ src/main/java/com/company/App.java
@@ -3,7 +3,9 @@ package com.company;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -17,14 +19,51 @@ public class App extends Application {
         stage.setX(600);
         stage.setY(300);
         stage.show();
-        Button worldButton = new Button ("World");
+        Menu fileMenu = new Menu("File");
+        MenuItem fileItemNew = new MenuItem("New (Ctrl + N)");
+        MenuItem fileItemOpen = new MenuItem("Open (Ctrl + O)");
+        MenuItem fileItemSave = new MenuItem("Save (Ctrl + S)");
+        MenuItem fileItemEmpty = new MenuItem("---");
+        MenuItem fileItemExit = new MenuItem("Exit Program");
+        fileMenu.getItems().add(fileItemNew);
+        fileMenu.getItems().add(fileItemOpen);
+        fileMenu.getItems().add(fileItemSave);
+        fileMenu.getItems().add(fileItemEmpty);
+        fileMenu.getItems().add(fileItemExit);
+        Menu editMenu = new Menu("Edit");
+        MenuItem editItemCut = new MenuItem("Cut (Ctrl + X)");
+        MenuItem editItemCopy = new MenuItem("Copy (Ctrl + C)");
+        MenuItem editItemPaste = new MenuItem("Paste (Ctrl + V)");
+        editMenu.getItems().add(editItemCut);
+        editMenu.getItems().add(editItemCopy);
+        editMenu.getItems().add(editItemPaste);
+        Menu runMenu = new Menu("Run");
+        MenuItem runItemCNR = new Menu("Compile and Run");
+        runMenu.getItems().add(runItemCNR);
+        Menu aboutMenu = new Menu("About");
+        MenuItem aboutItemAbout = new MenuItem ("About the app");
+        aboutMenu.getItems().add(aboutItemAbout);
+        MenuBar mbar = new MenuBar();
+        mbar.getMenus().add(fileMenu);
+        mbar.getMenus().add(editMenu);
+        mbar.getMenus().add(runMenu);
+        mbar.getMenus().add(aboutMenu);
+        Button clearButton = new Button ("Clear");
+        VBox topPanel = new VBox(mbar, clearButton);
+        TextArea textPanel = new TextArea();
         //worldButton.setTranslateY( 400 - worldButton.getHeight());
         //worldButton.setTranslateX( 600 - worldButton.getWidth());
-        Group group = new Group(new Button("Hello"), worldButton);
-        Scene scene = new Scene(group, 640, 480);
+        BorderPane bpane = new BorderPane();
+        bpane.setTop(topPanel);
+        bpane.setCenter(textPanel);
+        Scene scene = new Scene(bpane, 640, 480);
         stage.setScene(scene);
-        worldButton.setTranslateY( 480 - worldButton.getHeight());
-        worldButton.setTranslateX( 640 - worldButton.getWidth());
+        fileItemExit.setOnAction(e -> {
+            System.exit(0);
+        });
+        clearButton.setOnAction(actionEvent ->  {
+            textPanel.setText("");
+        });
     }
 
     public static void main(String args[]){
